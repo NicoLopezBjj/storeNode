b4aecbcc825f0bab6df056f4a1d965374be52f07
Nico <lopeznnr@gmail.com>
2023-09-26 10:38:17 -0300

    add db and conexion function

--- app.js
+++ app.js
@@ -2,11 +2,22 @@ const express=require('express')
 require('dotenv').config()
 
 const app = express()
+const connectDB=require('./db/conexion')
 
 const unPuerto= process.env.PUERTO
+
+const conexion = async () =>{
+    try {
+        await connectDB(process.env.MONGO_URL)
+        app.listen(unPuerto,console.log('servidor ejecutandose'))
+    }catch(error){
+        console.log(error)
+    }
+}
+
 
 app.get('/', (req,res)=>{
     res.send('hola mundo')
 })
 
-app.listen(unPuerto) 
+conexion()
