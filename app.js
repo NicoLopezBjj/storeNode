d8647d3d79cba532bdf76e3b188a30a992f073a1
Nico <lopeznnr@gmail.com>
2023-09-26 11:13:14 -0300

    install package async-errors

--- app.js
+++ app.js
@@ -1,23 +1,26 @@
 const express=require('express')
 require('dotenv').config()
+require('express-async-errors')
 
 const app = express()
 const connectDB=require('./db/conexion')
+const productsRouter=require('./routes/products')
 
 const unPuerto= process.env.PUERTO
 
 const conexion = async () =>{
     try {
-        await connectDB(process.env.MONGO_URL)
+        // await connectDB(process.env.MONGO_URL)
         app.listen(unPuerto,console.log('servidor ejecutandose'))
     }catch(error){
         console.log(error)
     }
 }
 
-
 app.get('/', (req,res)=>{
     res.send('hola mundo')
 })
+
+app.use('/shop',productsRouter)
 
 conexion()
--- routes/products.js
+++ routes/products.js
@@ -5,3 +5,5 @@ const {getAllProduct,getAllProductStatic}=require('../controllers/products')
 
 router.route('/').get(getAllProduct)
 router.route('/static').get(getAllProductStatic)
+
+module.exports=router
