011773ad9746a31d1d02b5c8651463fd0bdff34b
Nico <lopeznnr@gmail.com>
2023-09-26 11:28:33 -0300

    add models productsSchema

---
+++ models/product.js
@@ -0,0 +1,27 @@
+const mongoose= require ('mongoose')
+const productsSchema= new mongoose.Schema({
+    name:{
+        type: String,
+        required:[true,'nombre del producto']
+    },
+    price:{
+        type:Number,
+        required:[true,'precio del producot']
+    },
+    featured:{
+        type:Boolean,
+        default:false
+    },
+    rating:{
+        type:Number,
+        default:4.5
+    },
+    createdAt:{
+        type:Date,
+        default:Date.now()
+    },
+    company:{
+        type:String,
+        enum:['ikea','liddy','soniac','easy']
+    }
+})    
