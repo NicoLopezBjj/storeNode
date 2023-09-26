5e7a7ea9206d5c1677bf75d05da2d249df7c58d8
Nico <lopeznnr@gmail.com>
2023-09-26 10:47:14 -0300

    add controllers and routes

---
+++ controllers/products.js
@@ -0,0 +1,12 @@
+const getAllProduct=(req,res)=>{
+
+}
+
+const getAllProductStatic=(req,res)=>{
+    
+}
+
+module.exports={
+    getAllProduct,
+    getAllProductStatic
+}
---
+++ routes/products.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const router=express.routes()
+const {getAllProduct,getAllProductStatic}=require('../controllers/products')
+
+
+router.route('/').get(getAllProduct)
+router.route('/static').get(getAllProductStatic)
