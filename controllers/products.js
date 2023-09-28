bc301e6ab15ede9fe6733d06ea9e89f5100e82df
Nico <lopeznnr@gmail.com>
2023-09-28 09:50:02 -0300

    add unificar js and modificate controllers products.js

--- controllers/products.js
+++ controllers/products.js
@@ -1,9 +1,12 @@
+const Product = require('../models/product')
+
 const getAllProduct=(req,res)=>{
 
 }
 
-const getAllProductStatic=(req,res)=>{
-    
+const getAllProductStatic= async (req,res)=>{
+    const products= await Product.find()
+    res.status(200).json({products})
 }
 
 module.exports={
---
+++ unificar.js
@@ -0,0 +1,18 @@
+require('dotenv').config()
+
+const Product = require('./models/product')
+
+const connectDB = require('./db/conexion')
+
+const jsonProduct = require('./products.json')
+
+const iniciar= async () =>{
+    try{
+        await connectDB(process.env.MONGO_URL)
+        await Product.deleteMany()
+        await Product.create(jsonProduct)
+        console.log('SE CONECTO')
+    }catch(error){
+        console.log(error)
+    }
+}
