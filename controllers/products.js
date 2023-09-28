03ca518257ae37c6d2f44dff21a481f891f4dfb6
Nico <lopeznnr@gmail.com>
2023-09-28 10:45:51 -0300

    add req.query

--- controllers/products.js
+++ controllers/products.js
@@ -1,12 +1,15 @@
 const Product = require('../models/product')
 
-const getAllProduct=(req,res)=>{
-
+const getAllProduct= async (req,res)=>{
+    const products = await Product.find(req.query)
+    res.status(200).json(products)
 }
 
 const getAllProductStatic= async (req,res)=>{
-    const products= await Product.find()
-    res.status(200).json({products})
+    const products= await Product.find({featured:true})
+    res.status(200).json({products,numProducts:products.length})
+// para trabajar y buscar desde el back propiedades o campos de la base de datos
+// ejemplo estatico 
 }
 
 module.exports={
