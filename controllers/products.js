78af195f1062bff3a6857671c13cf42263ea109c
NicoLopezBjj <lopeznnr@gmail.com>
2023-10-03 09:31:54 -0300

    add products.ejs

--- controllers/products.js
+++ controllers/products.js
@@ -2,7 +2,9 @@ const Product = require('../models/product')
 
 const getAllProduct= async (req,res)=>{
     const products = await Product.find(req.query)
-    res.status(200).json(products)
+    // res.status(200).json({products})
+    res.render('products',{products:products})
+
 }
 
 const getAllProductStatic= async (req,res)=>{
--- routes/products.js
+++ routes/products.js
@@ -4,6 +4,8 @@ const {getAllProduct,getAllProductStatic}=require('../controllers/products')
 
 
 router.route('/').get(getAllProduct)
+// localhost:3600/shop
 router.route('/static').get(getAllProductStatic)
+// localhost:3600/shop/static
 
 module.exports=router
